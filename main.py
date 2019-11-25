8caa3673aa363942185422650927c4ad0ed13202
David <ricardo-david.valle-motard.1@ulaval.ca>
2019-11-25 07:15:30 -0500

    modification de la classe quoridorerror

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ class Quoridor:
         self.joueurs = joueurs 
         self.murs = murs
         #si chaine de caracteres, on cree un dictionnaire
-            if type(self.joueurs) is str:
+        if type(self.joueurs) is str:
             joueurs = [{'nom': self.joueurs[0],
-                    'murs': 10,
-                    'pos': (5, 1)},
-                    {'nom': self.joueurs[1],
-                    'murs': 10,
-                    'pos': (5, 9)},
-                    'murs': {'horizontaux': [],'verticaux': []}]
+            'murs': 10,
+            'pos': (5, 1)},
+            {'nom': self.joueurs[1],
+            'murs': 10,
+            'pos': (5, 9)},
+            'murs': {'horizontaux': [],'verticaux': []}]
             # si dictionnaire, on affecte les dictionnaires aux joueurs
-            if type(self.joueurs) is dict:
-                joueur1 = self.joueurs[0]
-                joueur2 = self.joueurs[1]
+        if type(self.joueurs) is dict:
+            joueur1 = self.joueurs[0]
+            joueur2 = self.joueurs[1]
         param_mur = {'horizontaux': [], 'verticaux': []} #dictionnaire des positions de murs. Pour linstant vide car aucun mur sur jeux
         #debut des classes error
         if hasattr(joueurs, '__iter__') is False:
             raise QuoridorError('Objet non itérable')
 class QuoridorError(Exception):
-    pass
+    def __init__(self, message):
+        super().__init__(message)

0fb63354a3e36d38c657f86cc3b72de174965184
David <57725336+caf3ine-addik@users.noreply.github.com>
2019-11-25 16:26:29 -0500

    fonction error permettant de trouver mur H ok

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 class Quoridor:
-    def __init__(self, joueurs, murs=None):
-        #initialise les variables joueurs et murs
-        self.joueurs = joueurs 
-        self.murs = murs
-        #si chaine de caracteres, on cree un dictionnaire
-        if type(self.joueurs) is str:
-            joueurs = [{'nom': self.joueurs[0],
-            'murs': 10,
-            'pos': (5, 1)},
-            {'nom': self.joueurs[1],
-            'murs': 10,
-            'pos': (5, 9)}]
-            # si dictionnaire, on affecte les dictionnaires aux joueurs
-        if type(self.joueurs) is dict:
-            joueur1 = self.joueurs[0]
-            joueur2 = self.joueurs[1]
-        param_mur = {'horizontaux': [], 'verticaux': []} #dictionnaire des positions de murs. Pour linstant vide car aucun mur sur jeux
-        #debut des classes error
-        if hasattr(joueurs, '__iter__') is False:
-            raise QuoridorError('Objet non itérable')
+    #erreur si non itérable
+    if hasattr(joueurs, '__iter__') is False:
+        raise QuoridorError
+    #erreur si plus de deux joueurs
+    if len(joueurs)>2:
+        raise QuoridorError
+    #erreur si plus de 10 murs par joueurs ou murs négatif
+    for i in range(2):
+        if joueurs[i].get('murs')<0 or joueurs[i].get('murs')>10:
+            raise QuoridorError
+    if joueurs[0].get('pos') != (5, 1) or joueurs[1].get('pos') =! (5, 9):
+        raise QuoridorError
+
+    #erreur si position joueur est sur mur horizontal existant.
+    for i in range(2):
+    for k in range(len(murs.get('horizontaux'))):
+        if joueurs[i].get('pos')[1] == murs.get('horizontaux')[k][1]:
+            for x in range(3):
+                if joueurs[i].get('pos')[0] == murs.get('horizontaux')[k][0] + x:
+                    raise QuoridorError
+                
+    #traitement erreur pour déplacement jeton
+    #erreur si déplacement en x et y out of bound
+    for i in range(2):
+        if joueurs[i].get('pos')[i]<1 or joueurs[i].get('pos')[i]>=10:
+            raise QuoridorError
+    
 class QuoridorError(Exception):
     def __init__(self, message):
         super().__init__(message)

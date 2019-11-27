e3f3ebd53746eabc2605c5177f58367f20da195e
Pierre-Luc Simard <pierre-luc.simard.10@ulaval.ca>
2019-11-26 19:53:53 -0500

    Update quoridor.py

--- quoridor.py
+++ quoridor.py
@@ -1,66 +1,111 @@
 import networkx as nx
 
+ 
+def non_itérable(joueurs): 
+    if iter(joueurs) != True:
+        raise QuoridorError("La valeur joueurs n'est pas itérable")
+            
 
+def nombre_itérable(joueurs):
+    if len(joueurs) != 2:
+        raise QuoridorError('Le nombre de joueurs est invalide')
+
+
+def nombre_murs(joueurs):
+    pass
+
+class QuoridorError(Exception):
+    pass
+
+
+# FONCTION FOURNIE
+def construire_graphe(joueurs, murs_horizontaux, murs_verticaux):
+    """
+    Crée le graphe des déplacements admissibles pour les joueurs.
+
+    :param joueurs: une liste des positions (x,y) des joueurs.
+    :param murs_horizontaux: une liste des positions (x,y) des murs horizontaux.
+    :param murs_verticaux: une liste des positions (x,y) des murs verticaux.
+    :returns: le graphe bidirectionnel (en networkX) des déplacements admissibles.
+    """
+    graphe = nx.DiGraph()
+
+    # pour chaque colonne du damier
+    for x in range(1, 10):
+        # pour chaque ligne du damier
+        for y in range(1, 10):
+            # ajouter les arcs de tous les déplacements possibles pour cette tuile
+            if x > 1:
+                graphe.add_edge((x, y), (x-1, y))
+            if x < 9:
+                graphe.add_edge((x, y), (x+1, y))
+            if y > 1:
+                graphe.add_edge((x, y), (x, y-1))
+            if y < 9:
+                graphe.add_edge((x, y), (x, y+1))
+
+    # retirer tous les arcs qui croisent les murs horizontaux
+    for x, y in murs_horizontaux:
+        graphe.remove_edge((x, y-1), (x, y))
+        graphe.remove_edge((x, y), (x, y-1))
+        graphe.remove_edge((x+1, y-1), (x+1, y))
+        graphe.remove_edge((x+1, y), (x+1, y-1))
+
+    # retirer tous les arcs qui croisent les murs verticaux
+    for x, y in murs_verticaux:
+        graphe.remove_edge((x-1, y), (x, y))
+        graphe.remove_edge((x, y), (x-1, y))
+        graphe.remove_edge((x-1, y+1), (x, y+1))
+        graphe.remove_edge((x, y+1), (x-1, y+1))
+
+    # retirer tous les arcs qui pointent vers les positions des joueurs
+    # et ajouter les sauts en ligne droite ou en diagonale, selon le cas
+    for joueur in map(tuple, joueurs):
+
+        for prédécesseur in list(graphe.predecessors(joueur)):
+            graphe.remove_edge(prédécesseur, joueur)
+
+            # si admissible, ajouter un lien sauteur
+            successeur = (2*joueur[0]-prédécesseur[0], 2*joueur[1]-prédécesseur[1])
+
+            if successeur in graphe.successors(joueur) and successeur not in joueurs:
+                # ajouter un saut en ligne droite
+                graphe.add_edge(prédécesseur, successeur)
+
+            else:
+                # ajouter les liens en diagonal
+                for successeur in list(graphe.successors(joueur)):
+                    if prédécesseur != successeur and successeur not in joueurs:
+                        graphe.add_edge(prédécesseur, successeur)
+
+    # ajouter les noeuds objectifs des deux joueurs
+    for x in range(1, 10):
+        graphe.add_edge((x, 9), 'B1')
+        graphe.add_edge((x, 1), 'B2')
+
+    return graphe  
+# FIN DE LA FONCTION FOURNIE
 
 class Quoridor:
 
     def __init__(self, joueurs, murs=None):
-        self.joueurs = joueurs
+        self.joueur1 = joueurs[0]
+        self.joueur2 = joueurs[1]
         self.murs = murs
-
-        if type(self.joueurs) == str:
-            if murs == None:
-                état = {'joueurs': 
-                    [{'nom': self.joueurs[0], 'murs': 10, 'pos': (5, 1)},
-                     {'nom': self.joueurs[1], 'murs': 10, 'pos': (5, 9)}],
-                     'murs': {'horizontaux': [],'verticaux': []}}
-            else:
-                état = {'joueurs': 
-                    [{'nom': self.joueurs[0], 'murs': 10, 'pos': (5, 1)},
-                     {'nom': self.joueurs[1], 'murs': 10, 'pos': (5, 9)}],
-                     'murs': {'horizontaux': [murs['horizontaux']],
-                     'verticaux': [murs['verticaux']]}}
-                
-        if type(self.joueurs) == dict:
-            if murs == None:
-                état = {'joueurs': [
-                    {'nom': self.joueurs['joueurs'][0]['nom'],
-                     'murs': self.joueurs['joueurs'][0]['murs'],
-                     'pos': self.joueurs['joueurs'][0]['pos']},
-                    {'nom': self.joueurs['joueurs'][1]['nom'],
-                     'murs': self.joueurs['joueurs'][1]['murs'],
-                     'pos': self.joueurs['joueurs'][0]['pos']}],
-                     'murs': {'horizontaux': [],'verticaux': []}}
-            else:
-                état = {'joueurs': [
-                    {'nom': self.joueurs['joueurs'][0]['nom'],
-                     'murs': self.joueurs['joueurs'][0]['murs'],
-                     'pos': self.joueurs['joueurs'][0]['pos']},
-                    {'nom': self.joueurs['joueurs'][1]['nom'],
-                     'murs': self.joueurs['joueurs'][1]['murs'],
-                     'pos': self.joueurs['joueurs'][0]['pos']}],
-                     'murs': {'horizontaux': [murs['horizontaux']],
-                     'verticaux': [murs['verticaux']]}}
-
-            
-         
+        if str(self.joueur1) == self.joueur1:
+            self.gamestate = {'joueurs':
+                            [{'nom': self.joueur1, 'murs': 10, 'pos': [5, 1]},
+                            {'nom': self.joueur2, 'murs': 10, 'pos': [5, 9]}],
+                            'murs': {'horizontaux': [], 'verticaux': []}}
+        else:
+                self.gamestate = {'joueurs':
+                                [self.joueur1, self.joueur2],
+                                'murs': {'horizontaux': [], 'verticaux': []}}
+        if type(self.murs) == dict:
+                self.gamestate['murs'] = self.murs
         
+
         """
-        Initialiser une partie de Quoridor avec les joueurs et les murs spécifiés, 
-        en s'assurant de faire une copie profonde de tout ce qui a besoin d'être copié.
-
-        :param joueurs: un itérable de deux joueurs dont le premier est toujours celui qui 
-        débute la partie. Un joueur est soit une chaîne de caractères soit un dictionnaire. 
-        Dans le cas d'une chaîne, il s'agit du nom du joueur. Selon le rang du joueur dans 
-        l'itérable, sa position est soit (5,1) soit (5,9), et chaque joueur peut initialement
-        placer 10 murs. Dans le cas où l'argument est un dictionnaire, celui-ci doit contenir 
-        une clé 'nom' identifiant le joueur, une clé 'murs' spécifiant le nombre de murs qu'il 
-        peut encore placer, et une clé 'pos' qui spécifie sa position (x, y) actuelle.
-        
-        :param murs: un dictionnaire contenant une clé 'horizontaux' associée à la liste des
-        positions (x, y) des murs horizontaux, et une clé 'verticaux' associée à la liste des
-        positions (x, y) des murs verticaux. Par défaut, il n'y a aucun mur placé sur le jeu.
-
         :raises QuoridorError: si joueurs n'est pas itérable.
         :raises QuoridorError: si l'itérable de joueurs en contient plus de deux.
         :raises QuoridorError: si le nombre de murs qu'un joueur peut placer est >10, ou négatif.
@@ -72,17 +117,53 @@ class Quoridor:
         
 
     def __str__(self):
-        """
-        Produire la représentation en art ascii correspondant à l'état actuel de la partie. 
-        Cette représentation est la même que celle du TP précédent.
-
-        :returns: la chaîne de caractères de la représentation.
-        """
+        
+        haut = f'Légende: 1={self.gamestate["joueurs"][0]["nom"]}, 2={self.gamestate["joueurs"][1]["nom"]}\n'
+        haut += '   -----------------------------------\n'
+        bas = '--|-----------------------------------\n'
+        bas += '  | 1   2   3   4   5   6   7   8   9'
+        liste_vide = []
+        for i in range(18,1,-1):
+            style_damier_1 = list(f"{i//2} | .   .   .   .   .   .   .   .   . |")
+            style_damier_2 = list("  |                                   |")
+            if i%2 == 0:
+                liste_vide.append(style_damier_1)
+            else:
+                liste_vide.append(style_damier_2)   
+        for i in range(2):
+            liste_vide[18-2*self.gamestate["joueurs"][i]["pos"][1]][4*self.gamestate["joueurs"][i]["pos"][0]] = f'{i+1}'
+        for i in range(len(self.gamestate["murs"]["horizontaux"])):
+            for j in range(7):
+                liste_vide[19-2*self.gamestate["murs"]["horizontaux"][i][1]][4*self.gamestate["murs"]["horizontaux"][i][0]+j-1] = '-'
+        for i in range(len(self.gamestate["murs"]["verticaux"])):
+            for j in range(3):
+                liste_vide[18-2*self.gamestate["murs"]["verticaux"][i][1]-j][4*self.gamestate["murs"]["verticaux"][i][0]-2] = '|'
+        damier = []
+        for ligne in liste_vide:
+            damier += ligne + ['\n']
+        a = ''.join(damier)
+        
+        return haut + a + bas
 
     def déplacer_jeton(self, joueur, position):
-        """
-        Pour le joueur spécifié, déplacer son jeton à la position spécifiée.
 
+        graphe = construire_graphe(
+        [joueur['pos'] for joueur in self.gamestate['joueurs']], 
+        self.gamestate['murs']['horizontaux'],
+        self.gamestate['murs']['verticaux'])
+        self.nouvelle_position = list(position)
+        self.gamestate['joueurs'][joueur - 1]['pos'] = self.nouvelle_position
+
+        if self.nouvelle_position != 1 or self.nouvelle_position != 2:
+            raise QuoridorError('le numéro du joueur doit être 1 ou 2')
+
+        if position[0] > 9 or position[0] < 1 or position[1] > 9 or position[1] < 1:
+            raise QuoridorError('la position est invalide (en dehors du damier)')
+        
+        if position not in list(graphe.successors((5,6))):
+            raise QuoridorError("la position est invalide pour l'état actuel du jeu.") 
+
+        """
         :param joueur: un entier spécifiant le numéro du joueur (1 ou 2).
         :param position: le tuple (x, y) de la position du jeton (1<=x<=9 et 1<=y<=9).
         :raises QuoridorError: le numéro du joueur est autre que 1 ou 2.
@@ -91,34 +172,22 @@ class Quoridor:
         """
 
     def état_partie(self):
-        """
-        Produire l'état actuel de la partie.
-
-        :returns: une copie de l'état actuel du jeu sous la forme d'un dictionnaire:
-        {
-            'joueurs': [
-                {'nom': nom1, 'murs': n1, 'pos': (x1, y1)},
-                {'nom': nom2, 'murs': n2, 'pos': (x2, y2)},
-            ],
-            'murs': {
-                'horizontaux': [...],
-                'verticaux': [...],
-            }
-        }
         
-        où la clé 'nom' d'un joueur est associée à son nom, la clé 'murs' est associée 
-        au nombre de murs qu'il peut encore placer sur ce damier, et la clé 'pos' est 
-        associée à sa position sur le damier. Une position est représentée par un tuple 
-        de deux coordonnées x et y, où 1<=x<=9 et 1<=y<=9.
+        return self.gamestate
 
-        Les murs actuellement placés sur le damier sont énumérés dans deux listes de
-        positions (x, y). Les murs ont toujours une longueur de 2 cases et leur position
-        est relative à leur coin inférieur gauche. Par convention, un mur horizontal se
-        situe entre les lignes y-1 et y, et bloque les colonnes x et x+1. De même, un
-        mur vertical se situe entre les colonnes x-1 et x, et bloque les lignes x et x+1.
-        """
+    def jouer_coup(self, joueur):  
 
-    def jouer_coup(self, joueur):
+        graphe = construire_graphe(
+        [joueur['pos'] for joueur in self.gamestate['joueurs']], 
+        self.gamestate['murs']['horizontaux'],
+        self.gamestate['murs']['verticaux'])
+
+        position_a_aller = nx.shortest_path(
+            graphe,
+            tuple(self.gamestate['joueurs'][joueur - 1]['pos']), f'B{joueur}')
+
+        self.déplacer_jeton(joueur, position_a_aller[1])
+
         """
         Pour le joueur spécifié, jouer automatiquement son meilleur coup pour l'état actuel 
         de la partie. Ce coup est soit le déplacement de son jeton, soit le placement d'un 
@@ -130,6 +199,14 @@ class Quoridor:
         """
 
     def partie_terminée(self):
+        
+        if self.gamestate['joueurs'][0]['pos'] == [5, 9]:
+            return self.gamestate['joueurs'][0]["nom"]
+        if self.gamestate['joueurs'][1]['pos'] == [5, 1]:
+            return self.gamestate['joueurs'][1]["nom"]
+        else:
+            return False
+        
         """
         Déterminer si la partie est terminée.
 
@@ -137,6 +214,12 @@ class Quoridor:
         """
 
     def placer_mur(self, joueur: int, position: tuple, orientation: str):
+        
+        self.gamestate['joueurs'][joueur-1]['murs'] = self.gamestate['joueurs'][joueur-1]['murs']-1
+        if orientation == 'horizontal':
+            self.gamestate['murs']['horizontaux'].append(position)
+        if orientation == 'vertical':
+            self.gamestate['murs']['verticaux'].append(position)
         """
         Pour le joueur spécifié, placer un mur à la position spécifiée.
 
@@ -148,5 +231,18 @@ class Quoridor:
         :raises QuoridorError: la position est invalide pour cette orientation.
         :raises QuoridorError: le joueur a déjà placé tous ses murs.
         """
+a = Quoridor([{'nom': 'raphael', 'murs': 3, 'pos': [5, 1]},
+              {'nom': 'jean-guy', 'murs': 6, 'pos': [5, 9]}], {'horizontaux': [], 'verticaux': []}
+             )
 
-class 
+
+
+
+
+print(a.état_partie())
+a.jouer_coup(2)
+print(a.état_partie())
+
+print(a)
+if a.partie_terminée() != False:
+    print(a.partie_terminée())

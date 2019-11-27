1aca289b71237fd0f919489498f7e7eaef69f1fd
rapre8 <rapre8@users.noreply.github.com>
2019-11-26 19:01:48 -0500

    Update quoridor.py

--- quoridor.py
+++ quoridor.py
@@ -1,6 +1,73 @@
 import networkx as nx
 
 
+# FONCTION FOURNIE
+def construire_graphe(joueurs, murs_horizontaux, murs_verticaux):
+    """
+    Crée le graphe des déplacements admissibles pour les joueurs.
+
+    :param joueurs: une liste des positions (x,y) des joueurs.
+    :param murs_horizontaux: une liste des positions (x,y) des murs horizontaux.
+    :param murs_verticaux: une liste des positions (x,y) des murs verticaux.
+    :returns: le graphe bidirectionnel (en networkX) des déplacements admissibles.
+    """
+    graphe = nx.DiGraph()
+
+    # pour chaque colonne du damier
+    for x in range(1, 10):
+        # pour chaque ligne du damier
+        for y in range(1, 10):
+            # ajouter les arcs de tous les déplacements possibles pour cette tuile
+            if x > 1:
+                graphe.add_edge((x, y), (x-1, y))
+            if x < 9:
+                graphe.add_edge((x, y), (x+1, y))
+            if y > 1:
+                graphe.add_edge((x, y), (x, y-1))
+            if y < 9:
+                graphe.add_edge((x, y), (x, y+1))
+
+    # retirer tous les arcs qui croisent les murs horizontaux
+    for x, y in murs_horizontaux:
+        graphe.remove_edge((x, y-1), (x, y))
+        graphe.remove_edge((x, y), (x, y-1))
+        graphe.remove_edge((x+1, y-1), (x+1, y))
+        graphe.remove_edge((x+1, y), (x+1, y-1))
+
+    # retirer tous les arcs qui croisent les murs verticaux
+    for x, y in murs_verticaux:
+        graphe.remove_edge((x-1, y), (x, y))
+        graphe.remove_edge((x, y), (x-1, y))
+        graphe.remove_edge((x-1, y+1), (x, y+1))
+        graphe.remove_edge((x, y+1), (x-1, y+1))
+
+    # retirer tous les arcs qui pointent vers les positions des joueurs
+    # et ajouter les sauts en ligne droite ou en diagonale, selon le cas
+    for joueur in map(tuple, joueurs):
+
+        for prédécesseur in list(graphe.predecessors(joueur)):
+            graphe.remove_edge(prédécesseur, joueur)
+
+            # si admissible, ajouter un lien sauteur
+            successeur = (2*joueur[0]-prédécesseur[0], 2*joueur[1]-prédécesseur[1])
+
+            if successeur in graphe.successors(joueur) and successeur not in joueurs:
+                # ajouter un saut en ligne droite
+                graphe.add_edge(prédécesseur, successeur)
+
+            else:
+                # ajouter les liens en diagonal
+                for successeur in list(graphe.successors(joueur)):
+                    if prédécesseur != successeur and successeur not in joueurs:
+                        graphe.add_edge(prédécesseur, successeur)
+
+    # ajouter les noeuds objectifs des deux joueurs
+    for x in range(1, 10):
+        graphe.add_edge((x, 9), 'B1')
+        graphe.add_edge((x, 1), 'B2')
+
+    return graphe  
+# FIN DE LA FONCTION FOURNIE
 
 class Quoridor:
 
@@ -78,7 +145,18 @@ class Quoridor:
         
         return self.gamestate
 
-    def jouer_coup(self, joueur):
+    def jouer_coup(self, joueur):  
+
+        graphe = construire_graphe(
+        [joueur['pos'] for joueur in self.gamestate['joueurs']], 
+        self.gamestate['murs']['horizontaux'],
+        self.gamestate['murs']['verticaux'])
+
+        position_a_aller = nx.shortest_path(
+            graphe,
+            tuple(self.gamestate['joueurs'][joueur - 1]['pos']), f'B{joueur}')
+
+        self.déplacer_jeton(joueur, position_a_aller[1])
 
         """
         Pour le joueur spécifié, jouer automatiquement son meilleur coup pour l'état actuel 
@@ -124,11 +202,15 @@ class Quoridor:
         :raises QuoridorError: le joueur a déjà placé tous ses murs.
         """
 a = Quoridor([{'nom': 'raphael', 'murs': 3, 'pos': [5, 1]},
-              {'nom': 'jean-guy', 'murs': 6, 'pos': [5, 9]}], {'horizontaux': [(2,2)], 'verticaux': []}
+              {'nom': 'jean-guy', 'murs': 6, 'pos': [5, 9]}], {'horizontaux': [], 'verticaux': []}
              )
 
+
+
+
+
 print(a.état_partie())
-a.placer_mur(1, (5,5), 'horizontal')
+a.jouer_coup(2)
 print(a.état_partie())
 
 print(a)
